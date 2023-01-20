e7d1bf2f7680e695e41cb363f1fc20e6705d0de0
Samandar <murodaliyevsamandarmurodaliyes@gmail.com>
2023-01-20 17:22:23 +0500

    added js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-// let namePrompt = prompt('ismingiz nima')
-// let surnamePrompt = prompt('familiyangiz nima')
-// let agePrompt = prompt('yoshingiz nechida')
-// let user = {}
-// user.name = namePrompt
-// user.surname = surnamePrompt
-// user.age = agePrompt
+let namePrompt = prompt('ismingiz nima')
+let surnamePrompt = prompt('familiyangiz nima')
+let agePrompt = prompt('yoshingiz nechida')
+let user = {}
+user.name = namePrompt
+user.surname = surnamePrompt
+user.age = agePrompt
 
-// console.log(user)
-var name = "Samandar"
-var surname = "Murodaliyev"
-var age = "20"
-var address = "Namangan"
-var education = "TUITKB"
-console.log(name,surname,age,address,education,)
+console.log(user)
+// var name = "Samandar"
+// var surname = "Murodaliyev"
+// var age = "20"
+// var address = "Namangan"
+// var education = "TUITKB"
+// console.log(name,surname,age,address,education,)
